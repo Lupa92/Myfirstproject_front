db61a8b72dd6c3fb33f11e499abfec4cb889aac9
Fernier <fernier.paul@gmail.com>
2025-09-19 15:56:11 +0200

    jhjh

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log("hello")
 fetch('https://myfirstproject-back.vercel.app/year')
 .then(response=>response.json())
 .then((data)=>{
-    document.querySelector("#year").textContent= year
+    document.querySelector("#year").textContent= data.year
 })
